6bfe57658aad547df528db3821353babc295ce30
VanshikaP <vanshikapundir92@gmail.com>
2020-07-24 11:23:01 -0700

    cleanup

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -92,34 +92,6 @@ class SortingRobot:
         """
         return self._light == "ON"
 
-    # def merge_in_place(self, arr, start, mid, end):
-    # # Your code here
-    #     start2 = mid
-    #     while start <= mid and start2 <= end:
-    #         if arr[start] <= arr[start2]:
-    #             start += 1
-    #         else:
-    #             index = start2
-    #             value = arr[start2]
-    #             while index is not start:
-    #                 arr[index] = arr[index - 1]
-    #                 index -= 1
-    #             arr[start] = value
-    #             start += 1
-    #             mid += 1
-    #             start2 += 1
-    #     return arr
-    
-    # def merge_sort_in_place(self,arr, l, r):
-    # # Your code here
-    #     if r - l < 1:
-    #         return arr
-    #     else:
-    #         m = (l + r) // 2
-    #         merge_sort_in_place(arr, l, m)
-    #         merge_sort_in_place(arr, m + 1, r)
-    #         return merge_in_place(arr, l, m+1, r)
-
     def merge_in_place(self, start, mid, end):
     # Your code here
         start2 = mid
